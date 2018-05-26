87075b296699ff97e4e06a02cbc9d40ff85b4051
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2018-05-26 10:27:52 +0200

    Refactor add-user

--- add-user.go
+++ add-user.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "gopkg.in/alecthomas/kingpin.v2"
-    "os"
+	"errors"
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
 )
 
 var addUserCommand *kingpin.CmdClause
@@ -12,35 +12,44 @@ var addUserName *string
 var addUserKeyFile *string
 
 func addAddUserCommand(app *kingpin.Application) {
-    addUserCommand = app.Command("add-user", "Add a user to the project")
-    addUserName = addUserCommand.Flag("name", "Name of user to add").Short('n').Default(os.Getenv("USER")).String()
-    addUserKeyFile = addUserCommand.Flag("key-file", "Filename of user's public key to add").Short('k').Default(os.Getenv("HOME") + "/.ssh/id_rsa.pub").String()
+	addUserCommand = app.Command("add-user", "Add a user to the project")
+	addUserName = addUserCommand.Flag("name", "Name of user to add").Short('n').Default(os.Getenv("USER")).String()
+	addUserKeyFile = addUserCommand.Flag("key-file", "Filename of user's public key to add").Short('k').Default(os.Getenv("HOME") + "/.ssh/id_rsa.pub").String()
+}
+
+func checkIfUserExists(teamPassFile *TeamPassFile, name string) error {
+	for i := 0; i < len(teamPassFile.Users); i++ {
+		if teamPassFile.Users[i].Name == name {
+			return errors.New(fmt.Sprintf("User already exists : %s", name))
+		}
+	}
+
+	return nil
 }
 
 func addUser() error {
-    var user TeamPassUser
+	var user TeamPassUser
 
-    teamPassFile, err := readFile(filename, true)
-    if err != nil {
-        return err
-    }
+	teamPassFile, err := readFile(filename, true)
+	if err != nil {
+		return err
+	}
 
-    keyValue, _, err := readPublicKey(addUserKeyFile)
-    if err != nil {
-        return err
-    }
+	keyValue, err := readPublicKey(addUserKeyFile)
+	if err != nil {
+		return err
+	}
 
-    for i := 0; i < len(teamPassFile.Users); i++ {
-        if teamPassFile.Users[i].Name == *addUserName {
-            return errors.New(fmt.Sprintf("User already exists : %s", *addUserName))
-        }
-    }
+	err = checkIfUserExists(&teamPassFile, *addUserName)
+	if err != nil {
+		return err
+	}
 
-    user.Name = *addUserName
-    user.Value = keyValue
-    user.Comment = *comment
+	user.Name = *addUserName
+	user.Value = keyValue
+	user.Comment = *comment
 
-    teamPassFile.Users = append(teamPassFile.Users, user)
+	teamPassFile.Users = append(teamPassFile.Users, user)
 
-    return writeFile(filename, false, teamPassFile)
+	return writeFile(filename, false, teamPassFile)
 }
