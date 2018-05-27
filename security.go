b957e94ac7563da8d328f39c6275894684de47cc
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2018-05-27 10:26:36 +0200

    - Added support for encryption and decryption of symmetric keys

--- security.go
+++ security.go
@@ -3,7 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 func readPublicKey(filename *string) (keyContent string, err error) {
@@ -18,8 +20,38 @@ func readPublicKey(filename *string) (keyContent string, err error) {
 	return
 }
 
-func CreateKey(len int) (result string, err error) {
-	cmd := exec.Command("openssl", "rand", fmt.Sprintf("%d", len))
+func createSymmetricalKey(bits int) (result string, err error) {
+	cmd := exec.Command("openssl", "rand", fmt.Sprintf("%d", bits))
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(bytes)))
+		return
+	}
+
+	return string(bytes), nil
+}
+
+func encryptSymmetricalKey(symKey string, publicKey string) (encSymKey string, err error) {
+	cmd := exec.Command("bash", "-c", "openssl rsautl -encrypt -oaep -pubin -inkey <(cat <<< \"$PUB_KEY\")")
+	cmd.Stdin = strings.NewReader(symKey)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("PUB_KEY=%s", publicKey),
+	)
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(bytes)))
+		return
+	}
+
+	return string(bytes), nil
+}
+
+func decryptSymmetricalKey(encSymKey string, privateKeyFile string) (symKey string, err error) {
+	cmd := exec.Command("openssl", "rsautl", "-decrypt", "-oaep", "-inkey", privateKeyFile)
+	cmd.Stdin = strings.NewReader(encSymKey)
+
 	bytes, err := cmd.CombinedOutput()
 	if err != nil {
 		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(bytes)))
