e00f03b9603b44dda8b0a378d44745b1d470057f
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2018-05-27 15:37:17 +0200

    - Added list values command

--- values.go
+++ values.go
@@ -7,9 +7,15 @@ import (
 )
 
 var valuesCommand *kingpin.CmdClause
+var valuesGroup *string
+var listValuesCommand *kingpin.CmdClause
 
 func setupValuesCommand(app *kingpin.Application) {
 	valuesCommand = app.Command("values", "Manage values")
+
+	valuesGroup = valuesCommand.Flag("group", "Name of group").Short('g').Default("_").String()
+
+	listValuesCommand = valuesCommand.Command("list", "List values")
 }
 
 func handleValuesCommand(commands []string) error {
@@ -18,7 +24,29 @@ func handleValuesCommand(commands []string) error {
 	}
 
 	switch commands[1] {
+	case "list":
+		return handleListValuesCommand(commands)
 	default:
 		return errors.New(fmt.Sprintf("Values subcommand not supported : %s", commands[1]))
 	}
+}
+
+func handleListValuesCommand(commands []string) error {
+	var group TeamPassGroup
+
+	teamPassFile, err := readFile(filename, true)
+	if err != nil {
+		return err
+	}
+
+	group, found := teamPassFile.Groups[*valuesGroup]
+	if !found {
+		return errors.New(fmt.Sprintf("Group not found : %s", valuesGroup))
+	}
+
+	for valueName, _ := range group.Values {
+		fmt.Printf("%s\n", valueName)
+	}
+
+	return writeFile(filename, false, teamPassFile)
 }
