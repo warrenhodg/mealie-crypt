57a63b3b1ee419f656003a7b69799012c2938d4d
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2018-05-27 13:26:41 +0200

    - Added support for encryption and decryption based on symmetrical key

--- security.go
+++ security.go
@@ -20,8 +20,9 @@ func readPublicKey(filename *string) (keyContent string, err error) {
 	return
 }
 
-func createSymmetricalKey(bits int) (result string, err error) {
-	cmd := exec.Command("openssl", "rand", fmt.Sprintf("%d", bits))
+func createSymmetricalKey(lenBits int) (result string, err error) {
+	lenBytes := lenBits / 8
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("openssl rand %d | base64", lenBytes))
 
 	bytes, err := cmd.CombinedOutput()
 	if err != nil {
@@ -51,6 +52,38 @@ func encryptSymmetricalKey(symKey string, publicKey string) (encSymKey string, e
 func decryptSymmetricalKey(encSymKey string, privateKeyFile string) (symKey string, err error) {
 	cmd := exec.Command("openssl", "rsautl", "-decrypt", "-oaep", "-inkey", privateKeyFile)
 	cmd.Stdin = strings.NewReader(encSymKey)
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(bytes)))
+		return
+	}
+
+	return string(bytes), nil
+}
+
+func encryptValue(symKey string, value string) (encValue string, err error) {
+	cmd := exec.Command("bash", "-c", "openssl aes-256-cbc -e -pass file:<(cat <<< $SYM_KEY | base64 -D)")
+	cmd.Stdin = strings.NewReader(value)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("SYM_KEY=%s", symKey),
+	)
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(bytes)))
+		return
+	}
+
+	return string(bytes), nil
+}
+
+func decryptValue(symKey string, value string) (decValue string, err error) {
+	cmd := exec.Command("bash", "-c", "openssl aes-256-cbc -d -pass file:<(cat <<< $SYM_KEY | base64 -D)")
+	cmd.Stdin = strings.NewReader(value)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("SYM_KEY=%s", symKey),
+	)
 
 	bytes, err := cmd.CombinedOutput()
 	if err != nil {
