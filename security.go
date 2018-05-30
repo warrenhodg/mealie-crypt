08622699676f5d36c718499d106591eaee5c38c3
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2018-05-30 10:35:30 +0200

    - Added environment passthru to called utils

--- security.go
+++ security.go
@@ -12,6 +12,7 @@ var keyLenBits = 256
 
 func readPublicKey(filename *string) (keyContent string, err error) {
 	cmd := exec.Command("ssh-keygen", "-e", "-f", *filename, "-m", "PKCS8")
+	cmd.Env = os.Environ()
 	pubkeyBytes, err := cmd.CombinedOutput()
 	if err != nil {
 		err = errors.New(fmt.Sprintf("%s : %s", err.Error(), string(pubkeyBytes)))
@@ -25,6 +26,7 @@ func readPublicKey(filename *string) (keyContent string, err error) {
 func createSymmetricalKey() (result string, err error) {
 	lenBytes := keyLenBits / 8
 	cmd := exec.Command("bash", "-c", fmt.Sprintf("openssl rand %d | base64", lenBytes))
+	cmd.Env = os.Environ()
 
 	bytes, err := cmd.CombinedOutput()
 	if err != nil {
@@ -38,7 +40,8 @@ func createSymmetricalKey() (result string, err error) {
 func encryptSymmetricalKey(symKey string, publicKey string) (encSymKey string, err error) {
 	cmd := exec.Command("bash", "-c", "openssl rsautl -encrypt -oaep -pubin -inkey <(cat <<< \"$PUB_KEY\")")
 	cmd.Stdin = strings.NewReader(symKey)
-	cmd.Env = append(os.Environ(),
+	cmd.Env = append(
+		os.Environ(),
 		fmt.Sprintf("PUB_KEY=%s", publicKey),
 	)
 
@@ -53,6 +56,7 @@ func encryptSymmetricalKey(symKey string, publicKey string) (encSymKey string, e
 
 func decryptSymmetricalKey(encSymKey string, privateKeyFile string) (symKey string, err error) {
 	cmd := exec.Command("openssl", "rsautl", "-decrypt", "-oaep", "-inkey", privateKeyFile)
+	cmd.Env = os.Environ()
 	cmd.Stdin = strings.NewReader(encSymKey)
 
 	bytes, err := cmd.CombinedOutput()
@@ -67,7 +71,8 @@ func decryptSymmetricalKey(encSymKey string, privateKeyFile string) (symKey stri
 func encryptValue(symKey string, value string) (encValue string, err error) {
 	cmd := exec.Command("bash", "-c", "openssl aes-256-cbc -e -pass file:<(cat <<< $SYM_KEY | base64 -D)")
 	cmd.Stdin = strings.NewReader(value)
-	cmd.Env = append(os.Environ(),
+	cmd.Env = append(
+		os.Environ(),
 		fmt.Sprintf("SYM_KEY=%s", symKey),
 	)
 
@@ -83,7 +88,8 @@ func encryptValue(symKey string, value string) (encValue string, err error) {
 func decryptValue(symKey string, value string) (decValue string, err error) {
 	cmd := exec.Command("bash", "-c", "openssl aes-256-cbc -d -pass file:<(cat <<< $SYM_KEY | base64 -D)")
 	cmd.Stdin = strings.NewReader(value)
-	cmd.Env = append(os.Environ(),
+	cmd.Env = append(
+		os.Environ(),
 		fmt.Sprintf("SYM_KEY=%s", symKey),
 	)
 
